d3271b6ecc0fa1764d416eb99d083385bc292470
javiedu <jafezca.edu@gmail.com>
2021-10-25 21:12:11 +0200

    Lluvia terminada, falta ajustar tiempos.

--- js/nivelUno.js
+++ js/nivelUno.js
@@ -2,6 +2,15 @@ var saltoUno=0;
 var saltoDos=0;
 var portalUnoActivo = false;
 var cofreAbierto = false;
+var contador = 0;
+var lluviaTerminada = false;
+var colisionLluvia = false;
+var tiempoLluvia = 0;
+var portalDosActivo = false;
+var posicionInicioX = 362;
+var posicionInicioY = 4900;
+
+var nombreObjetos = ['espadaUnFilo', 'espadaDosFilos', 'flecha', 'hacha', 'lanza', 'martillo', 'tridente']
 
 
 var mainState={ 
@@ -18,9 +27,20 @@ preload:function(){
     game.load.image('paredSangre','assets/paredSangre.png');
     game.load.image('casa','assets/casa.png');
     game.load.spritesheet('naruto','assets/naruto.png', 45, 63.75);
-    game.load.spritesheet('bowser','assets/monstruo.png', 139, 145);
+    game.load.spritesheet('monstruo','assets/monstruo.png', 139, 145);
     game.load.spritesheet('portal','assets/portal.png', 35, 140);
     game.load.spritesheet('cofre','assets/cofre.png', 38, 40);
+
+    //Lluvia
+    game.load.image('colisionLluvia','assets/colisionLluvia.png');
+    //Objetos lanzables
+    game.load.image('espadaUnFilo','assets/objetosLluvia/espadaUnFilo.png');
+    game.load.image('espadaDosFilos','assets/objetosLluvia/espadaDosFilos.png');
+    game.load.image('flecha','assets/objetosLluvia/flecha.png');
+    game.load.image('hacha','assets/objetosLluvia/hacha.png');
+    game.load.image('lanza','assets/objetosLluvia/lanza.png');
+    game.load.image('martillo','assets/objetosLluvia/martillo.png');
+    game.load.image('tridente','assets/objetosLluvia/tridente.png');
 },  
 
 create:function(){
@@ -32,7 +52,7 @@ this.paredSangre = game.add.sprite(660, 1938, 'paredSangre')
 
 this.plat = game.add.group()
 
-
+this.marcoColisionLluvia = game.add.sprite(0, 1938, 'colisionLluvia')
 this.suelos = game.add.group()
 this.game.world.setBounds(0, 0, 800, 5000)
 
@@ -71,11 +91,7 @@ this.plat.create(460,3013,'platDos');
 this.plat.create(550,3013,'platDos');
 this.plat.create(640,3013,'platDos');
 this.plat.create(730,3013,'platDos');
-//
-
 this.plat.create(182,2802,'platUno');
-//Muchas plataformas
-//610
 this.plat.create(0, 658,'platDos');
 this.plat.create(90, 658,'platDos');
 this.plat.create(180, 658,'platDos');
@@ -87,11 +103,13 @@ this.plat.create(630, 658,'platDos');
 this.plat.create(720, 658,'platDos');
 
 this.movil = game.add.sprite(400,4239,'platTres');
-
+//this.lluvia = game.add.group()
 this.portal = game.add.sprite(750, 3500, 'portal')
-this.bowser = game.add.sprite(650, 3467, 'bowser')
+this.portalDos = game.add.sprite(336, 2206, 'portal')
+
+this.monstruo = game.add.sprite(700, 3540, 'monstruo')
 this.cofre = game.add.sprite(390, 618, 'cofre')
-this.personaUno = game.add.sprite(360,3400,'naruto');/////////////////////////////////////
+this.personaUno = game.add.sprite(posicionInicioX, posicionInicioY, 'naruto');/////////////////////////////////////
 
 
 this.game.physics.arcade.enable(this.personaUno);
@@ -109,6 +127,15 @@ this.game.physics.arcade.enable(this.paredSangre)
 this.paredSangre.enableBody = true
 this.paredSangre.body.immovable = true
 
+this.game.physics.arcade.enable(this.marcoColisionLluvia)
+this.marcoColisionLluvia.enableBody = true
+this.marcoColisionLluvia.body.immovable = true
+this.marcoColisionLluvia.alpha = 0
+
+this.game.physics.arcade.enable(this.monstruo)
+this.monstruo.enableBody = true
+this.monstruo.body.immovable = true
+
 this.personaUno.body.gravity.y=1500;
 this.personaUno.body.setSize(this.personaUno.width-20,this.personaUno.height-21,10,10);
 this.personaUno.body.collideWorldBounds = true;
@@ -122,8 +149,13 @@ this.personaUno.animations.play('reposo');
 this.portal.animations.add('normal',[0, 1, 2, 3], 6, true);
 this.portal.animations.play('normal');
 
-this.bowser.animations.add('normal',[0, 1, 2, 3, 4, 5, 6, 7], 5, true);
-this.bowser.animations.play('normal');
+this.portalDos.animations.add('normal',[0, 1, 2, 3], 6, true);
+this.portalDos.animations.play('normal');
+
+this.monstruo.animations.add('andando',[0, 1, 2, 3, 4, 5, 6, 7], 10, true);
+this.monstruo.animations.play('andando');
+
+this.monstruo.anchor.setTo(0.5)
 
 
 this.game.physics.arcade.enable(this.suelos)
@@ -134,8 +166,16 @@ this.game.physics.arcade.enable(this.plat)
 this.plat.setAll('enableBody', true)
 this.plat.setAll('body.immovable', true)
 
+this.lluvia = game.add.group();
+this.lluvia.enableBody = true;
+this.lluvia.physicsBodyType = Phaser.Physics.ARCADE;
+this.lluvia.setAll('anchor.x', 0.5)
+
 this.game.physics.arcade.enable(this.portal)
 this.portal.alpha = 0;
+
+this.game.physics.arcade.enable(this.portalDos)
+this.portalDos.alpha = 0;
 
 this.game.physics.arcade.enable(this.cofre)
 this.cofre.animations.add('cerrado',[0],10,true);
@@ -145,7 +185,55 @@ game.camera.follow(this.personaUno)
 },
 
 update:function(){
+console.log(this.personaUno.body.x)
+console.log(this.personaUno.body.y)
 
+//Lluvia
+colisionLluvia = false
+this.game.physics.arcade.overlap(this.personaUno, this.marcoColisionLluvia, function(){
+    colisionLluvia = true
+})
+
+if(colisionLluvia){
+    tiempoLluvia += 1
+} else {
+    tiempoLluvia = 0
+}
+
+if(this.time.now > contador && colisionLluvia == true && lluviaTerminada == false){    
+    objeto = Math.floor((Math.random() * (6 - 0)) + 0)
+    posicion = Math.floor((Math.random() * (635 - 31)) + 31)
+    bala = this.lluvia.create(posicion, 1900, nombreObjetos[objeto])
+    if(bala){
+        bala.body.velocity.y = 200
+        contador = this.time.now + 1000
+    }
+}
+
+if(tiempoLluvia == 300){
+    lluviaTerminada = true
+    this.portalDos.alpha = 1
+}
+console.log(lluviaTerminada)
+
+if(lluviaTerminada == true){
+    this.game.physics.arcade.overlap(this.personaUno, this.portalDos, function(personaUno){
+        personaUno.body.y = 615
+        personaUno.body.x = 256
+    })
+    }
+
+
+//Monstruo
+if(this.monstruo.body.x > 600){
+    this.monstruo.body.velocity.x = -100
+    this.monstruo.scale.x = -1
+} else if(this.monstruo.body.x < 350){
+    this.monstruo.body.velocity.x = 100
+    this.monstruo.scale.x = 1
+}
+
+//Movimiento plataforma movil
 if(this.movil.body.y > 4186){
     this.movil.body.velocity.y = -100
 } else if(this.movil.body.y < 3900) {
@@ -154,34 +242,52 @@ if(this.movil.body.y > 4186){
 
 saltoUno = 1
 
-console.log(this.personaUno.body.y)
-
+//Colisiones
 this.game.physics.arcade.collide(this.personaUno, this.paredLateral)
-
 this.game.physics.arcade.collide(this.personaUno, this.paredSangre)
+
+this.game.physics.arcade.collide(this.personaUno, this.lluvia, function(personaUno, lluvia){
+    lluvia.kill()
+    personaUno.body.x = posicionInicioX
+    personaUno.body.y = posicionInicioY
+})
+
+this.game.physics.arcade.collide(this.lluvia, this.suelos, function(lluvia, suelo){
+    lluvia.kill()
+})
 
 this.game.physics.arcade.collide(this.personaUno,this.suelos,function() {
     saltoUno=1;
 });
-
 this.game.physics.arcade.collide(this.personaUno,this.plat,function() {
     saltoUno=1;
 });
-
 this.game.physics.arcade.collide(this.personaUno,this.movil, function() {
     saltoUno=1;
 });
 
-if(portalUnoActivo){
+this.game.physics.arcade.collide(this.personaUno, this.monstruo, function(personaUno, lluvia){
+    personaUno.body.x = posicionInicioX
+    personaUno.body.y = posicionInicioY
+})
+
+//Portal
+if(this.input.keyboard.isDown(Phaser.Keyboard.O)){
+    portalUnoActivo = true;
+    this.portal.alpha = 1;
+    this.monstruo.kill()
+}
+
+if(portalUnoActivo == true){
 this.game.physics.arcade.overlap(this.personaUno, this.portal, function(personaUno){
     personaUno.body.y = 2870
 })
 }
 
+//Cofre
 this.game.physics.arcade.overlap(this.personaUno, this.cofre, function(){
     cofreAbierto = true
 })
-
 
 if(cofreAbierto == true){
     this.cofre.animations.play('abierto')
@@ -189,17 +295,11 @@ if(cofreAbierto == true){
     this.cofre.animations.play('cerrado')
 }
 
-
-if(this.input.keyboard.isDown(Phaser.Keyboard.O)){
-    portalUnoActivo = true;
-    this.portal.alpha = 1;
-}
-
-
+//Movimiento personaje
 if (this.input.keyboard.isDown(Phaser.Keyboard.W) && saltoUno==1) {
     this.personaUno.body.y=this.personaUno.body.y-10;
     this.personaUno.body.velocity.y=-700;
-    console.log(this.personaUno.body.y)
+
 }
 if (this.input.keyboard.isDown(Phaser.Keyboard.A)) {
     this.personaUno.body.velocity.x=-200;
@@ -213,7 +313,7 @@ if (this.input.keyboard.isDown(Phaser.Keyboard.A)) {
 
 }
 
-
+//Animaciones personaje
 if (this.personaUno.body.velocity.x > 0) {
     this.personaUno.animations.play('derecha');
 }else if (this.personaUno.body.velocity.x < 0) {
